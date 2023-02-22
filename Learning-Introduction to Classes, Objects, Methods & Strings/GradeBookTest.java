b1e4f26fa1ca7c535d4122b833f7f220101e6e2f
ArulVScode <muhasrulmulis@gmail.com>
2023-02-23 00:32:11 +0700

    Add Program

---
+++ Learning-Introduction to Classes, Objects, Methods & Strings/GradeBookTest.java
@@ -0,0 +1,24 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Creating a GradeBook object and calling its displayMessage method.(package)
+ *
+ *User: Muh. Asrul Mulis
+ *Date: 20/February/2023
+ *
+ *Version(0.9)
+ */
+
+public class GradeBookTest {
+    // main method begins program execution
+	public static void main(String[] args) {
+		// create a GradeBook object and assign it to myGradeBook
+	     GradeBook myGradeBook = new GradeBook();
+		 
+		 myGradeBook.displayMessage();
+	} //end method
+} //end class GradeBookTest
