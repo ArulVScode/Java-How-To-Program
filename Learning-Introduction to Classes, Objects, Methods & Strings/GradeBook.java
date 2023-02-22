5ceac7a483245360dddca7aa73485c7377104bbc
ArulVScode <muhasrulmulis@gmail.com>
2023-02-23 00:31:09 +0700

    Add Program

--- Learning-Introduction to Classes, Objects, Methods & Strings/GradeBook.java
+++ Learning-Introduction to Classes, Objects, Methods & Strings/GradeBook.java
@@ -1,7 +1,22 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Class declaration with one method.(package)
+ *
+ *User: Muh. Asrul Mulis
+ *Date: 20/February/2023
+ *
+ *Version(0.9)
+ */
+
 public class GradeBook {
-    
+   // display a welcome message to the GradeBook user
 	public void displayMessage() {
 	    
 		System.out.println("Welcome to the Grade Book!");
-	}
-}
+	} //end method
+} // end class GradeBook
