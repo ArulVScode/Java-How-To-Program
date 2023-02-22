f523685886ec62673d9a1a10d9a29e7224f5a7bf
ArulVScode <muhasrulmulis@gmail.com>
2023-02-22 15:58:24 +0700

    Add Program

---
+++ Learning-Introduction to Java Aplications/WelcomeFour.java
@@ -0,0 +1,24 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Display multiple lines with method System.out.printf
+ *
+ *User: Muh. Asrul Mulis
+ *Date: 20/February/2023
+ *
+ *Version(0.4)
+ */
+
+//class name
+public class WelcomeFour {
+    //main method begins execution of java aplication
+   public static void main(String[] args) {
+        
+		System.out.printf("%s\n%s\n", 
+		"Welcome to", "Java Programming!");
+    } //end to method
+} //end to class
