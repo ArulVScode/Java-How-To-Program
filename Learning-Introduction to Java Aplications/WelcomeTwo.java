1cad92583330359ccfe4239f4ad5e02d28e16255
ArulVScode <muhasrulmulis@gmail.com>
2023-02-22 15:56:32 +0700

    Add Program

---
+++ Learning-Introduction to Java Aplications/WelcomeTwo.java
@@ -0,0 +1,20 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *User: Muh. Asrul Mulis
+ *Date: 20/February/2023
+ *
+ *Version(0.2)
+ */
+ //class name
+ public class WelcomeTwo {
+    //main method begins execution of java aplication
+	public static void main(String[] args) {
+	    System.out.print("Welcome to");
+		System.out.println("_Java Programming!");
+	} //end to method
+ } //end to class WelcomeTwo
