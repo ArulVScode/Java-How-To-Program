169b6b71019f64f8602d69b9f2b77ea01b193cbf
ArulVScode <muhasrulmulis@gmail.com>
2023-02-23 23:37:03 +0700

    Add Program

---
+++ Learning-Introduction to Classes, Objects, Methods & Strings/MaterialValues.java
@@ -0,0 +1,22 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Class declaration with one method that has a parameter.
+ *User: Muh. Asrul Mulis
+ *Date: 23/February/2023
+ *
+ *Version(0.2)
+ */
+
+public class MaterialValues {
+   // display a welcome message to the GradeBook user
+   public void displayMessage(String coursName ) {
+	   
+        System.out.printf("Welcome to the Grade Book for\n%s!\n", 
+		coursName );
+    } //end method displayMessage
+} //end class MaterialValues
