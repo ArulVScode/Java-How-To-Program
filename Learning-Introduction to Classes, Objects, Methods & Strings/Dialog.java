a04d324316f191fc40be4003515aafce79860822
ArulVScode <muhasrulmulis@gmail.com>
2023-02-25 15:41:19 +0700

    Add Program

---
+++ Learning-Introduction to Classes, Objects, Methods & Strings/Dialog.java
@@ -0,0 +1,23 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Using JOptionPane to display multiple lines in a dialog box.
+ *User: Muh. Asrul Mulis
+ *Date: 25/February/2023
+ *
+ *Version(0.4)
+ */
+
+import javax.swing.JOptionPane; // import class JOptionPane
+
+public class Dialog {
+     
+	public static void main(String[] args) {
+	     // display a dialog with a message
+		JOptionPane.showMessageDialog( null, "Welcome\nto\nJava");
+	} //end main
+} //end class Dialog
