6a2e3d403e676f7abc7902127af0d29be15e31e9
ArulVScode <muhasrulmulis@gmail.com>
2023-03-02 20:59:48 +0700

    Add Program

---
+++ Learning-Control Statements.1/Increment.java
@@ -0,0 +1,19 @@
+public class Increment {
+      
+	public static void main(String[] args) {
+	      
+		int c;
+		  
+		c =5;
+		System.out.println( c );
+		System.out.println( c++ );
+		System.out.println( c );
+		
+		System.out.println();
+		
+		c =5;
+		System.out.println( c );
+		System.out.println( ++c );
+		System.out.println( c );
+	}
+}
