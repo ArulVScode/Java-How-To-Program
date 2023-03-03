530c7a85a961101ede4017f75613fa6073ba2329
ArulVScode <muhasrulmulis@gmail.com>
2023-03-03 23:28:00 +0700

    Add Program

---
+++ Learning-Control Statements.1/MysteryTwo.java
@@ -0,0 +1,27 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Mystery Two
+ *User: Muh. Asrul Mulis
+ *Date: 03/Maret/2023
+ *
+ *Version(0.8)
+ */
+
+public class MysteryTwo {
+     
+	public static void main(String[] args) {
+	      
+		int count = 1;
+        
+        while(count <= 10) {
+		     
+			System.out.println( count %2 == 1? "*****" : "++++++++++");
+			++count;
+		} // end while
+	} // end main
+} // end class MysteryTwo
