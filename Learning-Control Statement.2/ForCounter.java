1c3f21edd60063c5aa3eab5d3bc7ed1c34302ac8
ArulVScode <muhasrulmulis@gmail.com>
2023-03-04 18:13:36 +0700

    Add Program

---
+++ Learning-Control Statement.2/ForCounter.java
@@ -0,0 +1,26 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Counter-controlled repetition with the for repetition statement.
+ *
+ *User: Muh. Asrul Mulis
+ *Date: 04/Maret/2023
+ *
+ *Version(0.2)
+ */
+
+public class ForCounter {
+     
+	public static void main(String[] args) {
+	     // for statement header includes initialization,
+         // loop-continuation condition and increment
+		for(int counter =1; counter <=10; counter++) {
+		     
+			System.out.printf("%d ", counter ); // output a new line
+		} // end for
+	} // end main
+} // end class ForCounter
