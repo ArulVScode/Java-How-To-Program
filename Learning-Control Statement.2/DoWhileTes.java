a9e9b72f5e03015170e721ef213817e2b0c5f2b3
ArulVScode <muhasrulmulis@gmail.com>
2023-03-06 21:48:58 +0700

    Add Program

---
+++ Learning-Control Statement.2/DoWhileTes.java
@@ -0,0 +1,31 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *do...while repetition statement.
+ *
+ *User: Muh. Asrul Mulis
+ *Date: 06/Maret/2023
+ *
+ *Version(0.5)
+ */
+
+public class DoWhileTes {
+     
+	public static void main(String[] args) {
+	      
+		int counter = 1; // initialize counter
+		
+		do {
+		     
+			System.out.printf("%d ", counter );
+			++counter; 
+		} // end do...while
+		while(counter <=10 );
+		
+		System.out.println(); // outputs a newline
+	} // end main
+} // end class DoWhileTes
