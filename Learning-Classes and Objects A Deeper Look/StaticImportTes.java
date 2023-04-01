d318286486649345bfa18c772739fe2b89cdfacb
ArulVScode <muhasrulmulis@gmail.com>
2023-04-01 21:24:34 +0700

    Add Program

---
+++ Learning-Classes and Objects A Deeper Look/StaticImportTes.java
@@ -0,0 +1,26 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAM
+ *
+ *Static import of Math class methods.
+ *User: Muh. Asrul Mulis
+ *Date: 01/April/2023
+ *
+ *Version(0.07)
+ */
+ 
+import static java.lang.Math.*;
+
+public class StaticImportTes {
+     
+	public static void main(String[] args) {
+	     
+		System.out.printf("sqrt(900.0) = %.1f\n", sqrt(900.0) );
+		System.out.printf("ceil(-9.8) = %.1f\n", ceil(-9.8) );
+		System.out.printf("E = %.1f\n", E );
+		System.out.printf("PI = %.1f\n", PI );
+	} // end main
+} // end class StaticImportTes
