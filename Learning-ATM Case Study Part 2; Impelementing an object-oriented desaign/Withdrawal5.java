6c55036ff5939683316703bc54aaad1c7ffeb34e
ArulVScode <muhasrulmulis@gmail.com>
2023-04-29 18:43:18 +0700

    Add Program

---
+++ Learning-ATM Case Study Part 2; Impelementing an object-oriented desaign/Withdrawal5.java
@@ -0,0 +1,17 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Class Withdrawal represents an ATM withdrawal transaction
+ *User: Muh. Asrul Mulis
+ *Date: 29/April/2023
+ *
+ *Version(5.1)
+ */
+ 
+public class Withdrawal5 extends Transcation {
+     
+} // end class Withdrawal5
