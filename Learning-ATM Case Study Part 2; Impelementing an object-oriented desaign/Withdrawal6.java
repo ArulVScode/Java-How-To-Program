471c4906d2abb1ad81a1579c633bf185fb79bba9
ArulVScode <muhasrulmulis@gmail.com>
2023-04-29 19:00:32 +0700

    Add Program

---
+++ Learning-ATM Case Study Part 2; Impelementing an object-oriented desaign/Withdrawal6.java
@@ -0,0 +1,28 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Withdrawal.java Generated using the class diagrams in Fig. 13.9 and Fig. 13.10
+ *User: Muh. Asrul Mulis
+ *Date: 29/April/2023
+ *
+ *Version(6.1)
+ */
+ 
+public class Withdrawal6 extends Transaction {
+     // attributes
+	private double amount; // amount to withdraw
+	private Keypad keypad; // reference to keypad
+	private CashDispenser cashDispenser; // reference to cash dispenser
+	 // no-argument constructor
+	public Withdrawal6() {
+	  
+	} // end no-argument Withdrawal6 constructor
+	@Override // method overriding execute
+	public void execute() {
+	 
+	} // end method execute
+} // end class Withdrawal6
