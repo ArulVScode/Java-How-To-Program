dc7b7b4a9fcfacf6982255c133c7bd8e988bda52
ArulVScode <muhasrulmulis@gmail.com>
2023-05-02 18:59:36 +0700

    Add Program

---
+++ Learning-ATM Case Study Part 2; Impelementing an object-oriented desaign/DepositSlot.java
@@ -0,0 +1,22 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *DepositSlot.java Represents the deposit slot of the ATM
+ *User: Muh. Asrul Mulis
+ *Date: 02/Mei/2023
+ *
+ *Version(0.6)
+ */
+ 
+public class DepositSlot {
+     // indicates whether envelope was received (always returns true,
+     // because this is only a software simulation of a real deposit slot)
+	public boolean isEnvelopeReceived() {
+		 
+		return true;
+	} // end method isEnvelopeReceived
+} // end class DepositSlot
