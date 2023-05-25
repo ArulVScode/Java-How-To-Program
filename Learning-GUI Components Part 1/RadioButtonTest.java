22752fa68fb2796bd5fbc7234af936e289d24552
ArulVScode <muhasrulmulis@gmail.com>
2023-05-25 21:42:38 +0700

    Add Program

---
+++ Learning-GUI Components Part 1/RadioButtonTest.java
@@ -0,0 +1,25 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Testing RadioButtonFrame.
+ *User: Muh. Asrul Mulis
+ *Date: 25/Mei/2023
+ *
+ *Version(2.6)
+ */
+import javax.swing.JFrame;
+
+public class RadioButtonTest {
+     
+	public static void main(String[] args) {
+	     
+		RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+		radioButtonFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		radioButtonFrame.setSize( 300, 100 ); // set frame size
+		radioButtonFrame.setVisible( true ); // display frame
+	} // end main
+} // end class RadioButtonTest
