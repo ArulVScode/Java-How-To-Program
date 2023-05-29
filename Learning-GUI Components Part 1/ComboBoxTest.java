c4e1656515aaaf785cf100df2c5dfc14fb189274
ArulVScode <muhasrulmulis@gmail.com>
2023-05-29 19:59:02 +0700

    Add Program

---
+++ Learning-GUI Components Part 1/ComboBoxTest.java
@@ -0,0 +1,25 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Testing ComboBoxFrame..
+ *User: Muh. Asrul Mulis
+ *Date: 29/Mei/2023
+ *
+ *Version(2.7)
+ */
+import javax.swing.JFrame;
+
+public class ComboBoxTest {
+     
+	public static void main(String[] args) {
+	     
+		ComboBoxFrame comboBoxFrame = new ComboBoxFrame();
+		comboBoxFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		comboBoxFrame.setSize( 350, 150 ); // set frame size
+		comboBoxFrame.setVisible( true ); // display frame
+	} // end main
+} // end class ComboBoxTest
