141c94d45829871d892b8d0ec4fffc6c178baa5e
ArulVScode <muhasrulmulis@gmail.com>
2023-07-14 14:53:33 +0700

    Add Program

---
+++ Learning-GUI Components Part 1/CalculatorTest.java
@@ -0,0 +1,23 @@
+/* 
+ *ArulVScode(Github)
+ *
+ *@muhasrulmulis(IG)
+ *
+ *JAVA HOW TO PROGRAMM
+ *
+ *Calculator.java GUI-learning
+ *User: Muh. Asrul Mulis
+ *Date: 14/juli/2023
+ *
+ *Version(2.20)
+ */ 
+
+import javax.swing.*;
+
+ public class CalculatorTest {
+      
+	public static void main(String[] args) {
+	     
+		new CalculatorFrame(); // display frame
+	}
+ }
